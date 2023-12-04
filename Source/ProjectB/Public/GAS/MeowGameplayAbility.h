04e55951b9e4de251d7a920d805a8c906df2cfd7
Jorge Casedas <50906822+JorgeCasedas@users.noreply.github.com>
2023-12-04 15:54:04 +0100

    Abilities c++ & new model

---
+++ Source/ProjectB/Public/GAS/MeowGameplayAbility.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/PBGameplayAbility.h"
+#include "MeowGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTB_API UMeowGameplayAbility : public UPBGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
